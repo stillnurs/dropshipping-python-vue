2afcb46fcd3e22cfa49a230228d9ae1ba2e93ef7
stillnurs <noorsultan.mamataliev@gmail.com>
2021-05-12 17:18:16 +0400

    reworked project structure

---
+++ dropship_vue/src/components/CartItem.vue
@@ -0,0 +1,72 @@
+<template>
+    <tr>
+        <td><router-link :to="item.product.get_absolute_url">{{ item.product.name }}</router-link></td>
+        <td>${{ item.product.price }}</td>
+        <td>
+            {{ item.quantity }}
+                <a @click="decrementQuantity(item)">
+                    <i class="fa fa-minus-circle"></i>
+                </a>
+            
+                 <a @click="incrementQuantity(item)">
+                    <i class="fa fa-plus-circle"></i>
+                </a>
+
+        </td>
+        <td>${{ getItemTotal(item).toFixed(2) }}</td>
+        <td><button class="delete" @click="removeFromCart(item)"></button></td>
+    </tr>
+</template>
+
+<script>
+export default {
+    name: 'CartItem',
+    props: {
+        initialItem: Object
+    },
+    data() {
+        return {
+            item: this.initialItem
+        }
+    },
+    methods: {
+        getItemTotal(item) {
+            return item.quantity * item.product.price
+        },
+        decrementQuantity(item) {
+            item.quantity -= 1
+
+            if (item.quantity === 0) {
+                this.$emit('removeFromCart', item)
+            }
+
+            this.updateCart()
+        },
+        incrementQuantity(item) {
+            item.quantity += 1
+
+            this.updateCart()
+        },
+        updateCart() {
+            localStorage.setItem('cart', JSON.stringify(this.$store.state.cart))
+        },
+        removeFromCart(item) {
+            this.$emit('removeFromCart', item)
+
+            this.updateCart()
+        },
+    },
+}
+</script>
+
+<style scoped>
+a {
+    text-decoration: none;
+}
+
+i.fa, .fas{
+    color:orange;
+    padding: 3px;
+}
+
+</style>
